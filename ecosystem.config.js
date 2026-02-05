818eede450a0793ceecb46b40d7687d9d87a76b4
Ronaldo Davi <ronaldodavi@gmail.com>
2026-02-05 18:26:04 -0300

    fix: allow gateway to start without config using --allow-unconfigured

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "openclaw",
     script : "openclaw",
-    args   : "gateway --port 18789",
+    args   : "gateway --port 18789 --allow-unconfigured",
     cwd    : "/home/openclaw",
     env: {
       NODE_ENV: "production",
